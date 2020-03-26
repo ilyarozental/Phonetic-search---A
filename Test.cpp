ccf224c55e0ca7f6441b96385d73567043a06a30
Moshe <moshe@mail.com>
2020-03-26 12:35:44 +0200

    1

--- Test.cpp
+++ Test.cpp
@@ -116,6 +116,7 @@ TEST_CASE("Test replacement of lower-case and upper-case") {
     CHECK(find(text, "HAPPI") == string("Happi"));
     CHECK(find(text, "HaPpI") == string("Happi"));  
 	CHECK(find(text, "happi") == string("Happi"));
+	
     CHECK(find(text, "Happi") == string("Happi"));
     CHECK(find(text, "HAPPI") == string("Happi"));
     CHECK(find(text, "HaPpI") == string("Happi"));
